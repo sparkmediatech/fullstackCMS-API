ae00898b2ad15b4c2c7dcce6cb3d48fc9fae1ffc
unknown <conventionalanimation@gmail.com>
2022-10-26 08:35:07 +0100

    redis url added from render

--- reditConnect.js
+++ reditConnect.js
@@ -6,10 +6,13 @@ const { promisify } = require("util");
 
 //const redis_client = redis.createClient();
 
-let redis_client = redis.createClient({
+/*let redis_client = redis.createClient({
     url: 'redis://redis:6379',
-})
+})*/
 
+let redis_client = redis.createClient({
+    url: 'redis://red-cdce5o02i3msb943nfog:6379',
+})
 
 redis_client.connect()
 
